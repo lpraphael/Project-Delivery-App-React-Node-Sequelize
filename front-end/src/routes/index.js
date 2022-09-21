3b09e3b7aecc516ed4ca069cfd90e1230e192c1a
lpraphael <raphael.perazzo@gmail.com>
2022-09-21 11:48:48 -0300

    feat: criando a rota "/" para login

--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -1,2 +1,17 @@
 import React from 'react';
-import {  }
+import { Routes as Switch, Route } from 'react-router-dom';
+import Login from '../pages/login';
+
+function Routes() {
+  return (
+    <Switch>
+      <Route
+        exact
+        path="/"
+        element={ <Login /> }
+      />
+    </Switch>
+  );
+}
+
+export default Routes;
