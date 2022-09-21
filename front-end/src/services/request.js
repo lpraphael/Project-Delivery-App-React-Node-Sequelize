764fd78679916bafa50a62805c10c2dda17bef5f
gabrieldezena10 <gabriel.deztor@gmail.com>
2022-09-21 18:08:48 -0300

    docs: add a comment regarding a doubt

--- front-end/src/services/request.js
+++ front-end/src/services/request.js
@@ -4,6 +4,7 @@ const API = axios.create({
   baseURL: 'http://localhost:3000',
 });
 
+// esperando o back para confirmar se é preciso usar as {}
 const requestRegister = async () => {
   const { data } = await API.post(url, body);
   return data;
