85b4c5da7b545b2495458b646b04101e793ec801
Calidor <victor.calidor@gmail.com>
2022-09-25 17:10:59 -0300

    Sales Route

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { productRouter, loginRouter, registerRouter } = require('../routes');
+const { productRouter, loginRouter, registerRouter, saleRouter } = require('../routes');
 
 const app = express();
 
@@ -8,6 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/coffee', (_req, res) => res.status(418).json('lalala'));
+app.use('/sales', saleRouter);
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
 app.use('/products', productRouter);
--- back-end/src/helpers/schemas.js
+++ back-end/src/helpers/schemas.js
@@ -5,18 +5,56 @@ const loginS = Joi.object({
     .email()
     .required(),
   password: Joi.string()
-  .min(6)
-  .required(),
+    .min(6)
+    .required(),
+});
+
+const createSaleS = Joi.object({
+  userId: Joi.number().required(),
+  sellerId: Joi.number().required(),
+  totalPrice: Joi.number().required(),
+  deliveryAddress: Joi.string().required(),
+  deliveryNumber: Joi.number().required(),
+  productsArray: Joi.array().items(
+    Joi.object(
+      { 
+        quantity: Joi.number().required(),
+        productId: Joi.number().required(),
+      },
+      ).required(),
+      ).required(),
+});
+
+const updateStatusS = Joi.object({
+  status: Joi.string().valid(
+    'Pendente',
+    'Preparando',
+    'Em Trânsito',
+    'Entregue',
+    ).required(),
 });
 
 const createUsersS = Joi.object({
-  email: Joi.string().email().required(),
-  role: Joi.string().valid('administrator', 'seller', 'customer'),
-  name: Joi.string().min(12).required(),
-  password: Joi.string().min(6).required(),
+  email: Joi.string()
+    .email()
+    .required(),
+  role: Joi.string()
+    .valid(
+      'administrator',
+      'seller',
+      'customer',
+    ),
+  name: Joi.string()
+    .min(12)
+    .required(),
+  password: Joi.string()
+    .min(6)
+    .required(),
 });
 
 module.exports = {
   loginS,
+  updateStatusS,
   createUsersS,
+  createSaleS,
 };
