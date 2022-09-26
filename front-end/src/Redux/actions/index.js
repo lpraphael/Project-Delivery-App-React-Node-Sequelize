e5548a9d1aecda98153a921906e56eb50f7288e9
lpraphael <raphael.perazzo@gmail.com>
2022-09-26 16:56:16 -0300

    feat: adicionando array de itens com quantidades ao estado global

--- front-end/src/Redux/actions/index.js
+++ front-end/src/Redux/actions/index.js
@@ -1,6 +1,12 @@
 export const VALIDATION_USER = 'VALIDATION_USER';
 export const REGISTER_NEW_USER = 'REGISTER_NEW_USER';
+export const SAVE_DRINKS = 'SAVE_DRINKS';
+export const QTY_DRINKS = 'QTY_DRINKS';
 
 export const actionUser = (value) => ({ type: VALIDATION_USER, value });
 
 export const registerNewUserAction = (value) => ({ type: REGISTER_NEW_USER, value });
+
+export const actionSaveDrinks = (value) => ({ type: SAVE_DRINKS, value });
+
+export const actionQtyDrinks = (value) => ({ type: QTY_DRINKS, value });
