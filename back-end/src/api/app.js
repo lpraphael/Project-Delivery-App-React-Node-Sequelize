0811f14373779300c52878652e3d3af29da6f4c3
Calidor <victor.calidor@gmail.com>
2022-09-26 15:06:07 -0300

    images route

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const { productRouter, loginRouter, registerRouter, saleRouter } = require('../routes');
-
 const app = express();
 
 app.use(cors());
@@ -12,5 +11,6 @@ app.use('/sales', saleRouter);
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
 app.use('/products', productRouter);
+app.use(express.static('public'));
 
 module.exports = app;
